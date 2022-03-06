024324eac642b1a11ed70e92961b247a57130c0c
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-03-05 19:53:44 -0600

    Add install.zsh and symlink.zsh scripts. install.zsh is the main install script, which calls symlink.zsh. zsh/.zshrc is a symlink created by symlink.zsh.

---
+++ symlink.zsh
@@ -0,0 +1,5 @@
+#!/usr/local/bin/zsh
+ln -sn ~/.dotfiles/zsh/env ~/.zshenv
+ln -sn ~/.dotfiles/zsh "$XDG_CONFIG_HOME"/zsh
+
+ln -sn ~/.dotfiles/zsh/zshrc ~/.dotfiles/zsh/.zshrc
---
+++ zsh/.zshrc
@@ -0,0 +1,1 @@
+/Users/Ethan/.dotfiles/zsh/zshrc
