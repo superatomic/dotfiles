a9d4cd4c406984249e3f93eae04cccdb3a30c396
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-03-05 23:25:50 -0600

    Link bash_profile and add iterm integration to bash

--- symlink.zsh
+++ symlink.zsh
@@ -2,4 +2,6 @@
 ln -sn ~/.dotfiles/zsh/env ~/.zshenv
 ln -sn ~/.dotfiles/zsh "$XDG_CONFIG_HOME"/zsh
 
+ln -sn ~/.dotfiles/bash/bash_profile ~/.bash_profile
+
 ln -sn ~/.dotfiles/zsh/zshrc ~/.dotfiles/zsh/.zshrc
