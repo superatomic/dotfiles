7fe856f3df683f95795d52ea0bbd70e264836c59
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-03-08 14:20:02 -0600

    Clean up and improve zsh symlinking. Symlinked all files in dotfiles/zsh instead of the directory itself, and made the zshrc to .zshrc symlink not exists within the dotfiles/zsh directory, but instead just $ZDOTDIR.

--- symlink.zsh
+++ symlink.zsh
@@ -1,10 +1,12 @@
 #!/usr/local/bin/zsh
 ln -sn ~/.dotfiles/zsh/env ~/.zshenv
-ln -sn ~/.dotfiles/zsh "$XDG_CONFIG_HOME"/zsh
+mkdir -p "$ZDOTDIR"
+for file in ~/.dotfiles/zsh/*; do
+  ln -sn "$file" "$ZDOTDIR"/"$file:t";
+done
+ln -sn "$ZDOTDIR"/zshrc "$ZDOTDIR"/.zshrc
 
 ln -sn ~/.dotfiles/bash/bash_profile ~/.bash_profile
-
-ln -sn ~/.dotfiles/zsh/zshrc ~/.dotfiles/zsh/.zshrc
 
 # Symlink all files in bin to the home bin folder.
 # Credit https://zaiste.net/posts/zsh-get-filename-extension-path/.
--- zsh/.zshrc
+++
@@ -1,1 +0,0 @@
-/Users/Ethan/.dotfiles/zsh/zshrc
