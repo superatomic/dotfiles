76084148747d957d7611a6be8a2f017bb4dbfc66
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-03-10 00:25:10 -0600

    Add docstrings to config/pythonrc.py and adjust spacing

--- config/pythonrc.py
+++ config/pythonrc.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+pythonrc.py ($PYTHONSTARTUP)
+Runs at python's interactive mode startup and provides convenient imports and functions.
+"""
+__author__ = 'Ethan Kinnear (https://github.com/superatomic)'
+
 import sys, os, asyncio, time
 from importlib import reload
 
@@ -7,6 +13,9 @@ def read_file(file_name, split_lines=False):
     """
     Convenience function when using python interactively.
     Used to get the contents of a file in a single line.
+
+    :param str file_name: The path to the file to read.
+    :param bool split_lines: If true, return a list of lines instead of one string.
     """
     with open(file_name) as file:
         if split_lines:
@@ -16,13 +25,13 @@ def read_file(file_name, split_lines=False):
 
 
 def _set_ps():
+    """Set the default python prompts to include color."""
     sys.ps1 = '\N{ESC}[33m' + '>>> ' + '\N{ESC}[0m'
     sys.ps2 = '\N{ESC}[33m' + '... ' + '\N{ESC}[0m'
 
+
 def _interactive_info():
-    """
-    Print all of the variables that have been made available.
-    """
+    """Print all of the variables that have been made available."""
     namespace = ', '.join(
         name
         for name in globals()
