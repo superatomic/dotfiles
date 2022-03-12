fa26a7975c50e18520b5af38e864f24e56ddb233
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-03-11 23:01:14 -0600

    Add starship and remove powerlevel10k Add config/starship.toml, and starship to Brewfile, and add starship to and remove powerlevel10k from zsh/sources.zsh

--- zsh/sources.zsh
+++ zsh/sources.zsh
@@ -1,14 +1,6 @@
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
 
 source "$XDG_DATA_HOME"/iterm2_shell_integration.zsh
 
-# Powerlevel
-source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme
 
 plugins=(zsh-autosuggestions colored-man-pages)
 
@@ -25,3 +17,7 @@ eval $(thefuck --alias)
 # * We can't run this until the heroku cli implements a fix to https://github.com/heroku/cli/issues/1956.
 # * Until then, this will be commented out.
 # HEROKU_AC_ZSH_SETUP_PATH=/Users/Ethan/Library/Caches/heroku/autocomplete/zsh_setup && test -f $HEROKU_AC_ZSH_SETUP_PATH && source $HEROKU_AC_ZSH_SETUP_PATH;
+
+
+# Must go at the end!
+eval "$(starship init zsh)"
