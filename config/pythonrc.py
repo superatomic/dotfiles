740d1511b6963b4496e6ec4005bb49a531eea668
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-03-11 19:58:52 -0600

    Set the location for python history to be in $XDG_STATE_HOME The default location is ~/.python_history, so it is changed to $XDG_STATE_HOME/python_history

--- config/pythonrc.py
+++ config/pythonrc.py
@@ -40,5 +40,38 @@ def _interactive_info():
     print('\N{ESC}[36m' + 'Added variables: ' + namespace + '\N{ESC}[0m')
 
 
+def _set_python_history_home():
+    """
+    Changes the location of python history from "~/.python_history" to "$XDG_STATE_HOME/python_history".
+    "$XDG_STATE_HOME/python_history" normally expands to "~/.local/state/python_history".
+    """
+    # HUGE thanks to https://unix.stackexchange.com/questions/630642/ for info on what to change.
+
+    from readline import read_history_file, write_history_file
+    from atexit import register
+
+    history_file = os.path.join(os.getenv('XDG_STATE_HOME'), 'python_history')
+
+    # Read Setup
+    try:
+        read_history_file(history_file)
+    except OSError:
+        print('\N{ESC}[31m' + f'OSError: Could not load {history_file}' + '\N{ESC}[0m')
+        info_message = (
+            'If this is your first time launching Python interactively,\n'
+            'history_file should be created at the next save.\n'
+        )
+        print('\N{ESC}[35m' + info_message + '\N{ESC}[0m')
+
+    # Write Setup
+    def write_history_file_atexit_hook():
+        try:
+            write_history_file(history_file)
+        except OSError:
+            print('\N{ESC}[31m' + f'OSError: Could not save to {history_file}' + '\N{ESC}[0m')
+    register(write_history_file_atexit_hook)
+
+
+_set_python_history_home()
 _set_ps()
 _interactive_info()
