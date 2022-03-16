3640b0f017822029043f7ab4c1da540705682e09
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-03-16 17:33:42 -0500

    Add commands to bin/dotfiles.zsh for editing the brew/Brewfile and itself These commands are `dotfiles brewfile` and `dotfiles self-edit`, respectfully.

--- bin/dotfiles.zsh
+++ bin/dotfiles.zsh
@@ -5,6 +5,10 @@ if [ "$1" = 'install' ]; then
   ~/.dotfiles/install.zsh
 elif [ "$1" = 'symlink' ]; then
   ~/.dotfiles/symlink.zsh
+elif [ "$1" = 'brewfile' ]; then
+  $EDITOR ~/.dotfiles/brew/Brewfile
+elif [ "$1" = 'self-edit' ]; then
+  $EDITOR "$0"
 elif [ "$1" = '' ]; then
   printmsg error "Error: Requires a command. Did you mean \"dotfiles install\"?" 1>&2
   exit 1
