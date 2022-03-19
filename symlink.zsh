c717f99d4bd2c3d072e6f85f4e188f0c5021bd7c
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-03-18 23:31:53 -0500

    Change quote spacing in symlink.zsh

--- symlink.zsh
+++ symlink.zsh
@@ -9,9 +9,9 @@ printmsg action "Symlinking files..."
 sln ~/.dotfiles/zsh/zshenv ~/.zshenv
 mkdir -p "$ZDOTDIR"
 for file in ~/.dotfiles/zsh/*; do
-  sln "$file" "$ZDOTDIR"/"$file:t";
+  sln "$file" "$ZDOTDIR/$file:t";
 done
-sln "$ZDOTDIR"/zshrc "$ZDOTDIR"/.zshrc
+sln "$ZDOTDIR/zshrc" "$ZDOTDIR/.zshrc"
 
 sln ~/.dotfiles/bash/bash_profile ~/.bash_profile
 
@@ -20,12 +20,12 @@ sln ~/.dotfiles/bash/bash_profile ~/.bash_profile
 # Credit https://zaiste.net/posts/zsh-get-filename-extension-path/.
 mkdir -p "$BIN_HOME"
 for file in ~/.dotfiles/bin/*(*); do
-  sln "$file" "$BIN_HOME"/"$file:t:r";
+  sln "$file" "$BIN_HOME/$file:t:r";
 done
 
 # Symlink all files in config into $XDG_CONFIG_HOME
 for file in ~/.dotfiles/config/*; do
-  sln "$file" "$XDG_CONFIG_HOME"/"$file:t";
+  sln "$file" "$XDG_CONFIG_HOME/$file:t";
 done
 
 printmsg success "Symlinked files successfully"
