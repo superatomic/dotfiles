5164ee7f888ce473bf79c963eb5252ffc773526b
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-04-09 23:22:58 -0500

    Update `config.fish`

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,10 @@
-eval "$(~/.local/share/cargo/bin/xshe fish)"
+~/.local/share/cargo/bin/xshe fish | source
 
 if status is-interactive
     set -x fish_greeting  # Set value to suppress message
+
+    thefuck --alias | source
     starship init fish | source
+
+    test -e {$XDG_DATA_HOME}/iterm2_shell_integration.fish ; and source {$XDG_DATA_HOME}/iterm2_shell_integration.fish
 end
