e601b8716d9093a58945f6e1d22face0cde4a279
Ethan Kinnear <contact@superatomic.dev>
2022-06-07 20:25:07 -0500

    Make `zsh` interactive in `init.sh`

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Install the dotfiles repository to ~/.dotfiles, and install it.
 git clone https://github.com/superatomic/dotfiles.git ~/.dotfiles
-~/.dotfiles/install.zsh
+zsh --interactive ~/.dotfiles/install.zsh
