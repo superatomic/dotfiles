bb49511d838f1777b4307480a66d8f8f9ff013d4
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-06-10 21:54:37 -0500

    Setup `fish` after installing rust

--- install.zsh
+++ install.zsh
@@ -16,11 +16,12 @@ if [[ $(uname) == "Darwin" ]]; then
   ~/.dotfiles/macos/install.zsh
   ~/.dotfiles/brew/install.zsh
 fi
-~/.dotfiles/fish/install.fish
 
 if ! type rustup >/dev/null; then
   peval "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
 fi
+
+~/.dotfiles/fish/install.fish
 
 printmsg success 'Finished dotfile installation!'
 
